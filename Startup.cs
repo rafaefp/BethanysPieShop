ce7085340eaed798f624a4a3c2002938c07b99be
rafaefp <rfpimentel@outlook.com>
2021-09-10 13:33:23 -0300

    Updating with addscoped shopping cart, addhttpcontextaccessor, addsession and app.usesession

--- Startup.cs
+++ Startup.cs
@@ -23,7 +23,10 @@ namespace BethanysPieShop
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddScoped<IPieRepository, PieRepository>();
-            services.AddScoped<ICategoryRepository, CategoryRepository>();            
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+            services.AddHttpContextAccessor();
+            services.AddSession();
             services.AddControllersWithViews();
         }
 
@@ -37,6 +40,7 @@ namespace BethanysPieShop
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseSession();
 
             app.UseRouting();            
 
