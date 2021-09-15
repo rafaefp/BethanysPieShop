447c95144843a1be524a420ecddc0a4f9d22f29d
rafaefp <rfpimentel@outlook.com>
2021-09-15 11:30:48 -0300

    Adding order and orderDetails dbsets

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -1,8 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BethanysPieShop.Models
 {
@@ -16,6 +12,8 @@ namespace BethanysPieShop.Models
         public DbSet<Pie> Pies { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
