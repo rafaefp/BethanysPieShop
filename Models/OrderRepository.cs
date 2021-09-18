45257658e0ed159664a35a53d269ad8e29763bc1
rafaefp <rfpimentel@outlook.com>
2021-09-18 14:55:59 -0300

    Updating CreateOrder method.

--- Models/OrderRepository.cs
+++ Models/OrderRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BethanysPieShop.Models
 {
@@ -16,24 +17,24 @@ namespace BethanysPieShop.Models
         public void CreateOrder(Order order)
         {
             order.OrderPlaced = DateTime.Now;
-
-            _appDbContext.Orders.Add(order);
-
+            
             var shoppingCartItems = _shoppingCart.ShoppingCartItems;
+            order.OrderTotal = _shoppingCart.GetShoppingCartTotal();
+            order.OrderDetails = new List<OrderDetail>();
 
             foreach (var shoppingCartItem in shoppingCartItems)
             {
-                var orderDetail = new OrderDetail()
+                var orderDetail = new OrderDetail
                 {
                     Amount = shoppingCartItem.Amount,
                     PieId = shoppingCartItem.Pie.PieId,
-                    OrderId = order.OrderId,
                     Price = shoppingCartItem.Pie.Price
                 };
 
-                _appDbContext.OrderDetails.Add(orderDetail);
+                order.OrderDetails.Add(orderDetail);
             }
-
+            
+            _appDbContext.Orders.Add(order);
             _appDbContext.SaveChanges();
         }
     }
